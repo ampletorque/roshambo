63938fb29ec5d44c8f222abfec4ad9578da8a9f8
james robert williams & andrew david burt <student@epicodus.com>
2015-04-22 11:50:50 -0700

    wrote second spec, pass second spec

--- lib/roshambo.rb
+++ lib/roshambo.rb
@@ -1,6 +1,9 @@
 class String
   define_method(:beats?) do |opponent|
-  self.eql?("rock") && opponent.eql?("scissors")
+  if self.eql?(opponent)
+    "draw"
+  elsif self.eql?("rock") && opponent.eql?("scissors")
+    true
    end
-
+  end
  end
--- spec/roshambo_spec.rb
+++ spec/roshambo_spec.rb
@@ -6,4 +6,7 @@ describe('String#roshambo') do
   it("returns true if rock is the object and scissors is the argument") do
     expect("rock".beats?("scissors")).to(eq(true))
   end
+  it("returns message 'draw' if the object and arugument are the same") do
+    expect("rock".beats?("rock")).to(eq("draw"))
+end
 end
